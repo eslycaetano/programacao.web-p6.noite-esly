eb135e1953bb792ad65c4d24316f802fd7e3533b
Esly Caetano <eslyeslycaetano2014@gmail.com>
2024-09-15 00:24:16 -0300

    Ajustando algumas coisas no css e no js.

--- Atividade03/main.js
+++ Atividade03/main.js
@@ -9,16 +9,17 @@ document.getElementById('btnConsulta').addEventListener('click', async () => {
         const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         if (!response.ok) throw new Error('Erro na consulta ao ViaCEP');
         
-        const data = await response.json();
-        if (data.erro) {
+        const dado = await response.json();
+        if (dado.erro) {
             alert('CEP não encontrado.');
             return;
         }
-
-        document.getElementById('logradouro').textContent = `Logradouro: ${data.logradouro}`;
-        document.getElementById('bairro').textContent = `Bairro: ${data.bairro}`;
-        document.getElementById('cidade').textContent = `Cidade: ${data.localidade}`;
-        document.getElementById('estado').textContent = `Estado: ${data.uf}`;
+        document.getElementById('cep').value = dado.cep;
+        document.getElementById('logradouro').innerHTML = `<strong> Logradouro: </strong> ${dado.logradouro}`;
+        document.getElementById('bairro').innerHTML = `<strong> Bairro: </strong> ${dado.bairro}`;
+        document.getElementById('cidade').innerHTML = `<strong> Cidade: </strong> ${dado.localidade}`;
+        document.getElementById('estado').innerHTML = `<strong> Estado: </strong> ${dado.uf}`;
+        document.getElementById('regiao').innerHTML = `<strong> Região: </strong> ${dado.regiao}`;
     } catch (error) {
         alert('Erro ao consultar o CEP. Tente novamente.');
     }
