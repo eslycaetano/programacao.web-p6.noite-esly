59e3d01e5f555f9d8d42aebe2dddb562f0dcc33c
Esly Caetano <eslyeslycaetano2014@gmail.com>
2024-09-26 12:02:03 -0300

    Adiciona arquivos inicias da Atividade04
    Ainda não foi respondida, precisa do componente principal.

---
+++ Atividade04/src/App.jsx
@@ -0,0 +1,11 @@
+import { CounterColateral } from "./components/CounterColateral";
+
+
+export function App() {
+  return (
+    <div>
+      {/* importe aqui o componente que você deseja */}
+      <CounterColateral />
+    </div>
+  )
+}
