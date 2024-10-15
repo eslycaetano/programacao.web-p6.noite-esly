b4315411bbb272c55324ca1f37325a4f23ee33e5
Esly Caetano <eslyeslycaetano2014@gmail.com>
2024-10-15 20:45:39 -0300

    Adicionando, a url das imagens nos animes.

--- Atividade06/backend/data/animeData.js
+++ Atividade06/backend/data/animeData.js
@@ -4,70 +4,70 @@ let animes = [
       nome: "Dragon Ball Z",
       genero: ["Artes marciais", "Aventura", "Fantasia", "Shounen"],
       estudio: ["Toei Animation"],
-      imagem: "url_da_imagem_hunter_x_hunter"
+      imagem: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hCUyiimqo0FjWakvEl2Yu6UNW69.jpg"
     },
     {
       id: 2,
       nome: "Hunter x Hunter (2011)",
       genero: ["Ação", "Aventura", "Fantasia", "Shounen"],
       estudio: ["Madhouse"],
-      imagem: "url_da_imagem_hunter_x_hunter"
+      imagem: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/tolQj5yffSxkEGXusPwNcvYrbph.jpg"
     },
     {
       id: 3,
       nome: "One Piece",
       genero: ["Ação", "Aventura", "Fantasia", "Shounen"],
       estudio: ["Toei Animation"],
-      imagem: "url_da_imagem_hunter_x_hunter"
+      imagem: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yBYDzzmNoT5uXkXo3NGatLIANYK.jpg"
     },
     {
       id: 4,
       nome: "Sousou no Frieren",
       genero: ["Drama", "Aventura", "Fantasia", "Shounen"],
       estudio: ["Madhouse"],
-      imagem: "url_da_imagem_hunter_x_hunter"
+      imagem: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qykTIEJQF8cunKD0CKOqab15wns.jpg"
     },
     {
       id: 5,
       nome: "Haikyuu!!",
       genero: ["Esporte", "Comédia", "Drama", "Shounen"],
       estudio: ["Production I.G"],
-      imagem: "url_da_imagem_hunter_x_hunter"
+      imagem: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ecQ84z2sR0XapZu435MnFuEBzD8.jpg"
     },
     {
       id: 6,
       nome: "Death Note",
       genero: ["Mistério", "Drama", "Thriller", "Shounen"],
       estudio: ["Madhouse"],
-      imagem: "url_da_imagem_hunter_x_hunter"
+      imagem: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/iigTJJskR1PcjjXqxdyJwVB3BoU.jpg"
     },
     {
       id: 7,
       nome: "Yu Yu Hakusho",
       genero: ["Artes marciais", "Aventura", "Sobrenatural", "Shounen"],
       estudio: ["Studio Pierrot"],
-      imagem: "url_da_imagem_hunter_x_hunter"
+      imagem: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/j6dzDPkrAsfJ6GUyyMPz38ZkpbF.jpg"
     },
     {
       id: 8,
       nome: "Jujutsu Kaisen",
       genero: ["Ação", "Aventura", "Sobrenatural", "Shounen"],
       estudio: ["MAPPA"],
-      imagem: "url_da_imagem_hunter_x_hunter"
+      imagem: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fHpKWq9ayzSk8nSwqRuaAUemRKh.jpg"
     },
     {
       id: 9,
       nome: "Fullmetal Alchemist: Brotherhood",
       genero: ["Ação", "Aventura", "Fantasia", "Shounen"],
       estudio: ["Bones"],
-      imagem: "url_da_imagem_hunter_x_hunter"
+      imagem: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8H4ej2NpujYVBPsW2smmzC8d2xU.jpg"
     },
     {
       id: 10,
       nome: "Vinland Saga",
       genero: ["Aventura", "Drama", "Histórico", "Seinen"],
       estudio: ["Wit Studio", "MAPPA"],
-      imagem: "url_da_imagem_hunter_x_hunter"
+      imagem: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9WZwylQ4VuLXaN45bMtxnztItUa.jpg"
     },
   ];
   
